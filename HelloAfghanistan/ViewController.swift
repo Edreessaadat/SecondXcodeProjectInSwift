20ac9ddf3168b933c8b1ae88181af84ed6fd3e0a
Mohammad Edrees <edrees_saadat@yahoo.com>
2020-11-01 00:03:06 -0400

    Initial Commit

---
+++ HelloAfghanistan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloAfghanistan
+//
+//  Created by Mohammad Edrees on 10/8/20.
+//  Copyright © 2020 Mohammad Edrees. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
